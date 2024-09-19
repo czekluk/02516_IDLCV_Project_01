d8f89944b6c9d87c07d04091b53860c8a60d04ad
Filip Penzar <fpenzar@gmail.com>
2024-09-19 22:36:14 +0200

    Add saving of the experiments

--- src/main.py
+++ src/main.py
@@ -3,6 +3,43 @@ from data.make_dataset import HotdogNotHotDog_DataModule
 from data.custom_transforms import base_transform
 import torch
 import torch.nn as nn
+import json
+import os
+
+
+PROJECT_BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+def save_results(outputs):
+    """
+    Saves the best model from outputs (the parameters).
+    Saves all of the results into a results/experiments.json
+    """
+    experiments_path = os.path.join(PROJECT_BASE_DIR, "results/experiments.json")
+    saved_models_path = os.path.join(PROJECT_BASE_DIR, "results/saved_models")
+    # save the best model
+    best_model = outputs[0]
+    torch.save(best_model["model"].state_dict(), 
+               os.path.join(saved_models_path, f"{best_model['test_acc'][-1]:.4f}-{best_model['model_name']}.pth"))
+    
+    # save the results
+    try:
+        with open(experiments_path, "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        data = []
+    # stringify the model and optimizer from the outputs as they are not json serializable
+    for output in outputs:
+        if "model" in output:
+            output["model"] = str(output["model"])
+        if "optimizer" in output["optimizer_config"]:
+            output["optimizer_config"]["optimizer"] = str(output["optimizer_config"]["optimizer"])
+    
+    data.extend(outputs)
+    # sort the entries
+    data = sorted(data, key=lambda x: x['test_acc'][-1], reverse=True)
+    with open(experiments_path, "w") as f:
+        json.dump(data, f, indent=4)
 
 
 def main():
@@ -17,11 +54,11 @@ def main():
         {"optimizer": torch.optim.Adam, "params": {"lr": 1e-3}},
         {"optimizer": torch.optim.SGD, "params": {"lr": 1e-2, "momentum": 0.9}}
     ]
-    epochs = [2]
+    epochs = [1]
     
     trainer = Trainer(models, optimizers, epochs, trainloader, testloader)
     outputs = trainer.train()
-    print(outputs)
+    save_results(outputs)
 
 if __name__ == "__main__":
     main()
