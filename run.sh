d72dc34ed7128f216c31e81e0522d69411e08395
Nandor Takacs <nandortakacs123@gmail.com>
2024-09-22 17:46:39 +0200

    bash script to be run on dtu cluster

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
-## General options
-## –- specify queue --
+# #!/bin/sh
+# # General options
+# # –- specify queue --
 # BSUB -q gpuv100
 # ## -- set the job Name --
 # BSUB -J deep learning in computer vision training
@@ -9,7 +9,7 @@
 # ## -- Select the resources: 1 gpu in exclusive process mode --
 # BSUB -gpu "num=1:mode=exclusive_process"
 # ## -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
-# BSUB -W 00:25
+# BSUB -W 03:00
 # request 5GB of system-memory
 # BSUB -R "rusage[mem=5GB]"
 # ## -- set the email address --
@@ -24,11 +24,21 @@
 # ## -- -o and -e mean append, -oo and -eo mean overwrite --
 # BSUB -o gpu_%J.out
 # BSUB -e gpu_%J.err
-# -- end of LSF options --
+# #-- end of LSF options --
 
 nvidia-smi
 # Load the cuda module
-module load cuda/11.6
+
+
+module unload python
+module load python3
+unset PYTHONPATH
+unset PYTHONHOME
+# pip3 install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
+module load matplotlib 
+pip3 install tqdm 
+module load cuda/11.6 
+module load numpy 
 
 /appl/cuda/11.6.0/samples/bin/x86_64/linux/release/deviceQuery
 
@@ -40,3 +50,4 @@ python ./src/main.py > "$LOG_FILE" 2>&1
 
 # Print a message indicating that the logs have been saved
 echo "Logs have been saved to $LOG_FILE"
+
